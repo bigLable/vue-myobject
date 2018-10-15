17474ce8e4a6b35cab1c4ff35da34c0b20c3f6d2
bigYang <1216102533@qq.com>
2018-10-15 11:49:48 +0800

    10/15  11.37

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
+Vue.use(ElementUI)
 import routers from './routes'
 const router =new VueRouter({
   routers:routers,
