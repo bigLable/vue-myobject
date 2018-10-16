fa24ce6d1650a32ab3f34e12f67c4775a57805c0
yuki <momojuncn@yahoo.co.jp>
2018-10-16 10:15:19 +0800

    tate

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <app-shops></app-shops>
     <router-link to="/Car">去往购物车</router-link>
+    <router-link to="/repair">wx</router-link>
     <router-view></router-view>
   </div>
 
@@ -11,11 +11,6 @@
   import Shops from './components/goods--zhang/shops/shops'
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
   components:{
     'app-shops':Shops,
 
--- src/routerinstall.js
+++ src/routerinstall.js
@@ -5,11 +5,11 @@ export const routerinstall = [
   {path:'/repair',component:repair},
   {path:'/search',component:search},
   {path:'/shops',component:shops},
-      {path:':id/edit',component:UserEdit,name:'edit',beforeEnter:(to,from,next)=>{
-          console.log('守卫开始工作！');
-          next({path:'/'});    //不写或者参数是false时，路由中止，如果参数是指定的一个路由时，将会跳转
-        }},
-  {path:'*',redirect:'/'}
+  //     {path:':id/edit',component:UserEdit,name:'edit',beforeEnter:(to,from,next)=>{
+  //         console.log('守卫开始工作！');
+  //         next({path:'/'});    //不写或者参数是false时，路由中止，如果参数是指定的一个路由时，将会跳转
+  //       }},
+  // {path:'*',redirect:'/'}
     ]
 
 
