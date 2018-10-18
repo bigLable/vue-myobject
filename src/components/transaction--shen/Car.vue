c0fb64bbebc9285f1cd2ff4e9a97404f147e290e
bigYang <1216102533@qq.com>
2018-10-18 10:19:53 +0800

    10/18  10:19

--- src/components/transaction--shen/Car.vue
+++ src/components/transaction--shen/Car.vue
@@ -2,6 +2,7 @@
  <div>
 
    <!--<car-head></car-head>-->
+   <pay-oderprocess></pay-oderprocess>
    <car-cardata></car-cardata>
    <!--<router-view></router-view>-->
    <!--<car-foot></car-foot>-->
@@ -11,12 +12,14 @@
   import tsCarhead from './tsCar'
   import tsCardata from './tsCardata'
   import  foot from './Carfoot'
+  import oderprocess from './oderprocess'
     export default {
         name: "Car",
         components:{
           'car-head':tsCarhead,
           'car-cardata':tsCardata,
-           'car-foot':foot
+           'car-foot':foot,
+          'pay-oderprocess':oderprocess,
         }
     }
 </script>
