f011fa3d834fe8da33cef9bde9cb20f9bb2753ec
yuki <momojuncn@yahoo.co.jp>
2018-10-18 14:55:58 +0800

    nmntys

--- src/components/goods--zhang/repair/assembly-r/raputor/chizu.vue
+++ src/components/goods--zhang/repair/assembly-r/raputor/chizu.vue
@@ -1,35 +1,44 @@
 <template>
+
   <div id="dv1" class="row">
   <div class="container">
     <div class="p90"></div>
-    <div class="row">
-      <form class="form-inline" action="" id="fm1">
-      <select class="form-control" form="fm1">
-        <option>北京</option>
-        <option>山东</option>
-        <option>河南</option>
-        <option>江苏</option>
-        <option>广西壮族自治区</option>
-      </select>
-      <input type="button" class="btn btn-primary" form="fm1" value="确定">
+    <div class=" row ">
+      <div class="col-lg-3 col-md-4 col-sm-5 col-xs-5 ">
+        <form class="form-inline" action="" id="fm1">
+          <select class="form-control" form="fm1">
+            <option>北京</option>
+            <option>山东</option>
+            <option>河南</option>
+            <option>江苏</option>
+            <option>广西壮族自治区</option>
+          </select>
+        </form>
+      </div>
+
+      <div class=" row col-lg-6 col-md-4  col-sm-3 col-xs-3">
+        <input type="button" class="btn btn-primary" form="fm1" value="确定">
+      </div>
+      <div class=" row col-lg-3 col-md-4  col-xs-4">
         <input type="button" class="btn btn-warning" value="快速定位到我的位置">
-      </form>
-    </div>
+      </div>
 
-  </div>
-    </div>
 
+    </div>
+</div>
+    </div>
 </template>
 
 <script>
     export default {
         name: "chizu"
     }
+
 </script>
 
 <style scoped>
 .form-control{
-  width: 400px;
+  width: 250px;
   height: 60px;
   background: #000000;
   color: white;
@@ -38,14 +47,14 @@
 }
   .btn.btn-primary{
     height: 60px;
-    width: 170px;
+    width: 120px;
     font-size: 20px;
   }
   .btn.btn-warning{
     height: 60px;
-    width: 400px;
+    width: 250px;
     font-size: 20px;
-    float: right;
+
   }
   .p90{
     height: 30px;
--- src/components/goods--zhang/repair/assembly-r/raputor/raputor.vue
+++ src/components/goods--zhang/repair/assembly-r/raputor/raputor.vue
@@ -3,104 +3,78 @@
     <div  id=ddd class="container">
       <div id=reputor class="row">
         <div class=" repudiv col-lg-3 col-md-3 col-sm-6 col-xs-6" >
-          <div class="imgdiv">
-            <img src="" alt="">
-          </div>
-          <div>
-            <h3>{{}}</h3>
-            <p>地区:{{}}</p>
-            <p>地址:{{}}</p>
-            <p>营业时间:{{}}</p>
-            <p>电话:{{}}</p>
-          </div>
-        </div>
-        <div class=" repudiv col-lg-3 col-md-3 col-sm-6 col-xs-6" >
-          <div class="imgdiv">
-            <img src="" alt="">
-          </div>
-          <div>
-            <h3>{{}}</h3>
-            <p>地区:{{}}</p>
-            <p>地址:{{}}</p>
-            <p>营业时间:{{}}</p>
-            <p>电话:{{}}</p>
+          <div class="bbb">
+            <div class="imgdiv">
+              <img class="img-responsive" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1539855396615&di=8b3a6dc81961ebb3d1941de100f44f5e&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F15%2F45%2F91%2F27g58PICNEG_1024.png" alt="">
+            </div>
+            <div>
+              <h3>{{}}</h3>
+              <p>地区:{{}}</p>
+              <p>地址:{{}}</p>
+              <p>营业时间:{{}}</p>
+              <p>电话:{{}}</p>
+            </div>
           </div>
         </div>
         <div class=" repudiv col-lg-3 col-md-3 col-sm-6 col-xs-6" >
-          <div class="imgdiv">
-            <img src="" alt="">
-          </div>
-          <div>
-            <h3>{{}}</h3>
-            <p>地区:{{}}</p>
-            <p>地址:{{}}</p>
-            <p>营业时间:{{}}</p>
-            <p>电话:{{}}</p>
-          </div>
-        </div>
-        <div class=" repudiv col-lg-3 col-md-3 col-sm-6 col-xs-6" >
-          <div class="imgdiv">
-            <img src="" alt="">
-          </div>
-          <div>
-            <h3>{{}}</h3>
-            <p>地区:{{}}</p>
-            <p>地址:{{}}</p>
-            <p>营业时间:{{}}</p>
-            <p>电话:{{}}</p>
+          <div class="bbb">
+            <div class="imgdiv">
+              <img class="img-responsive" src="https://www.sonystyle.com.cn/content/dam/sonystyle/products/lenses/e_lens/sel24f14gm/product/img_sel24f14gm_b.jpg.thumb.537.537.png" alt="">
+            </div>
+            <div>
+              <h3>{{}}</h3>
+              <p>地区:{{}}</p>
+              <p>地址:{{}}</p>
+              <p>营业时间:{{}}</p>
+              <p>电话:{{}}</p>
+            </div>
           </div>
         </div>
         <div class=" repudiv col-lg-3 col-md-3 col-sm-6 col-xs-6" >
-          <div class="imgdiv">
-            <img src="" alt="">
-          </div>
-          <div>
-            <h3>{{}}</h3>
-            <p>地区:{{}}</p>
-            <p>地址:{{}}</p>
-            <p>营业时间:{{}}</p>
-            <p>电话:{{}}</p>
-          </div>
-        </div>
-        <div class=" repudiv col-lg-3 col-md-3 col-sm-6 col-xs-6" >
-          <div class="imgdiv">
-            <img src="" alt="">
-          </div>
-          <div>
-            <h3>{{}}</h3>
-            <p>地区:{{}}</p>
-            <p>地址:{{}}</p>
-            <p>营业时间:{{}}</p>
-            <p>电话:{{}}</p>
+          <div class="bbb">
+            <div class="imgdiv">
+              <img class="img-responsive" src="https://www.sonystyle.com.cn/content/dam/sonystyle/products/lenses/e_lens/sel24f14gm/product/img_sel24f14gm_b.jpg.thumb.537.537.png" alt="">
+            </div>
+            <div>
+              <h3>{{}}</h3>
+              <p>地区:{{}}</p>
+              <p>地址:{{}}</p>
+              <p>营业时间:{{}}</p>
+              <p>电话:{{}}</p>
+            </div>
           </div>
         </div>
         <div class=" repudiv col-lg-3 col-md-3 col-sm-6 col-xs-6" >
-          <div class="imgdiv">
-            <img src="" alt="">
-          </div>
-          <div>
-            <h3>{{}}</h3>
-            <p>地区:{{}}</p>
-            <p>地址:{{}}</p>
-            <p>营业时间:{{}}</p>
-            <p>电话:{{}}</p>
-          </div>
-        </div>
-        <div class=" repudiv col-lg-3 col-md-3 col-sm-6 col-xs-6" >
-          <div class="imgdiv">
-            <img src="" alt="">
-          </div>
-          <div>
-            <h3>{{}}</h3>
-            <p>地区:{{}}</p>
-            <p>地址:{{}}</p>
-            <p>营业时间:{{}}</p>
-            <p>电话:{{}}</p>
+          <div class="bbb">
+            <div class="imgdiv">
+              <img class="img-responsive" src="https://www.sonystyle.com.cn/content/dam/sonystyle/products/lenses/e_lens/sel24f14gm/product/img_sel24f14gm_b.jpg.thumb.537.537.png" alt="">
+            </div>
+            <div>
+              <h3>{{}}</h3>
+              <p>地区:{{}}</p>
+              <p>地址:{{}}</p>
+              <p>营业时间:{{}}</p>
+              <p>电话:{{}}</p>
+            </div>
           </div>
         </div>
         <div class=" repudiv col-lg-3 col-md-3 col-sm-6 col-xs-6" >
+          <div class="bbb">
+            <div class="imgdiv">
+              <img class="img-responsive" src="https://www.sonystyle.com.cn/content/dam/sonystyle/products/lenses/e_lens/sel24f14gm/product/img_sel24f14gm_b.jpg.thumb.537.537.png" alt="">
+            </div>
+            <div>
+              <h3>{{}}</h3>
+              <p>地区:{{}}</p>
+              <p>地址:{{}}</p>
+              <p>营业时间:{{}}</p>
+              <p>电话:{{}}</p>
+            </div>
+          </div>
+        </div><div class=" repudiv col-lg-3 col-md-3 col-sm-6 col-xs-6" >
+        <div class="bbb">
           <div class="imgdiv">
-            <img src="" alt="">
+            <img class="img-responsive" src="https://www.sonystyle.com.cn/content/dam/sonystyle/products/lenses/e_lens/sel24f14gm/product/img_sel24f14gm_b.jpg.thumb.537.537.png" alt="">
           </div>
           <div>
             <h3>{{}}</h3>
@@ -110,6 +84,37 @@
             <p>电话:{{}}</p>
           </div>
         </div>
+      </div>
+        <div class=" repudiv col-lg-3 col-md-3 col-sm-6 col-xs-6" >
+          <div class="bbb">
+            <div class="imgdiv">
+              <img class="img-responsive" src="https://www.sonystyle.com.cn/content/dam/sonystyle/products/lenses/e_lens/sel24f14gm/product/img_sel24f14gm_b.jpg.thumb.537.537.png" alt="">
+            </div>
+            <div>
+              <h3>{{}}</h3>
+              <p>地区:{{}}</p>
+              <p>地址:{{}}</p>
+              <p>营业时间:{{}}</p>
+              <p>电话:{{}}</p>
+            </div>
+          </div>
+        </div>
+        <div class=" repudiv col-lg-3 col-md-3 col-sm-6 col-xs-6" >
+          <div class="bbb">
+            <div class="imgdiv">
+              <img class="img-responsive" src="https://www.sonystyle.com.cn/content/dam/sonystyle/products/lenses/e_lens/sel24f14gm/product/img_sel24f14gm_b.jpg.thumb.537.537.png" alt="">
+            </div>
+            <div>
+              <h3>{{}}</h3>
+              <p>地区:{{}}</p>
+              <p>地址:{{}}</p>
+              <p>营业时间:{{}}</p>
+              <p>电话:{{}}</p>
+            </div>
+          </div>
+        </div>
+
+
       </div>
   </div>
 
@@ -129,10 +134,9 @@
 
   }
 .imgdiv{
-  height: 190px;
+  width: 100px;
+  height: 150px;
   background: black;
-  margin-top: 30px;
-  border-radius: 10px;
 }
   .repudiv{
     height: 400px;
@@ -140,6 +144,13 @@
   #dv1{
     background: #f6f8f8;
     height: 100%;
+  }
+  .bbb{
+ padding-left: 10px;
+    padding-right: 10px;
+    margin-top: 10px;
+
+    border: 1px solid #cccccc;
   }
 
 </style>
