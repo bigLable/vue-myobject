dca8ee682fe06ac133de6ea4a49e2bdac0acc9e4
zyx <1115811239@qq.com>
2018-10-21 12:39:44 +0800

    上传数据

--- src/components/user--zhai/registe.vue
+++ src/components/user--zhai/registe.vue
@@ -3,92 +3,38 @@
     <div class="main">
       <div class="login"><h1>注 册</h1></div>
       <div class="login-1"><h5>Welcome to registration</h5></div>
-      <div class="input">
-        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="demo-ruleForm">
-          <el-form-item
-            prop="name"
-            :rules="[
-             { required: true, message: '用户名不能为空', trigger: 'blur' }]">
-            <el-input v-model="ruleForm.name" placeholder="请输入用户名"></el-input>
-          </el-form-item>
-        </el-form>
-          <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" class="demo-ruleForm">
-            <el-form-item prop="pass">
-              <el-input type="password" v-model="ruleForm2.pass" autocomplete="off" placeholder="请输入密码"></el-input>
-            </el-form-item>
-            <el-form-item  prop="checkPass">
-              <el-input type="password" v-model="ruleForm2.checkPass" autocomplete="off" placeholder="再次输入密码"></el-input>
-            </el-form-item>
-          </el-form>
-          <el-form :model="dynamicValidateForm" ref="dynamicValidateForm" class="demo-dynamic">
-            <el-form-item
-              prop="email"
-              :rules="[
-                { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-                { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
-                      ]">
-              <el-input v-model="dynamicValidateForm.email" placeholder="请输入邮箱"></el-input>
-            </el-form-item>
-            <el-button type="primary" style="width:250px">注 册</el-button>
-            <router-link role="presentation" to="/login"><el-button type="info" style="width:250px" id="btn">已经有账号</el-button></router-link>
-          </el-form>
-      </div>
+      <el-input v-model="user" placeholder="请输入用户名" type="text" class="input"></el-input><br><br>
+      <el-input v-model="pwd" placeholder="请输入密码" type="text" class="input"></el-input><br><br>
+      <el-input v-model="email" placeholder="请输入邮箱" type="text" class="input"></el-input><br><br>
+      <el-button type="primary" style="width:250px" @click="getdata" class="input">注 册</el-button><br><br>
+      <router-link role="presentation" to="/login"><el-button type="info" style="width:250px" id="btn">已经有账号</el-button></router-link>
     </div>
   </div>
 </template>
 
 <script>
+  import $ from 'jquery'
   export default {
     name: "registe",
     data() {
-      var validatePass = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请输入密码'));
-        } else {
-          if (this.ruleForm2.checkPass !== '') {
-            this.$refs.ruleForm2.validateField('checkPass');
-          }
-          callback();
-        }
-      };
-      var validatePass2 = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请再次输入密码'));
-        } else if (value !== this.ruleForm2.pass) {
-          callback(new Error('两次输入密码不一致!'));
-        } else {
-          callback();
-        }
-      };
       return {
-        input: '',
-        input2:'',
-        input3:'',
-        input4:'',
-        input5:'',
-        ruleForm2: {
-          pass: '',
-          checkPass: '',
-          age: ''
-        },
-        rules2: {
-          pass: [
-            { validator: validatePass, trigger: 'blur' }
-          ],
-          checkPass: [
-            { validator: validatePass2, trigger: 'blur' }
-          ],
-      },
-        ruleForm: {
-          name: '',
-        },
-        dynamicValidateForm: {
-          domains: [{
-            value: ''
-          }],
-          email: ''
-        }
+        user:'',
+        pwd:'',
+        email:'',
       };
+    },
+    methods:{
+     getdata(){
+       let _this=this;
+       $.post('http://localhost:3000/users/addUsers',
+         {
+           userName:_this.user,
+           userPwd:_this.pwd,
+           userEmail:_this.email
+         },function(res){
+           alert('开始注册ok！')
+         })
+     }
     }
   }
 </script>
@@ -114,12 +60,11 @@
   }
   .input{
     width:250px;
-    height:250px;
-    margin:0 auto;
-    margin-top:70px;
+    height:40px;
+    margin-left:150px;
   }
   #btn{
     margin-top:20px;
-    margin-left:-1px;
+    margin-left:150px;
   }
 </style>
