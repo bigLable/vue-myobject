daadfe7ea1b4ca59905a89bd1e27cafd51fb183a
zyx <1115811239@qq.com>
2018-10-24 15:30:58 +0800

    完善登录注册功能

--- src/components/user--zhai/registe.vue
+++ src/components/user--zhai/registe.vue
@@ -3,53 +3,99 @@
     <div class="main">
       <div class="login"><h1>注 册</h1></div>
       <div class="login-1"><h5>Welcome to registration</h5></div>
-      <el-input id="name" v-model="user" placeholder="请输入用户名" type="text" class="input" maxlength="8" minlength="2"></el-input><br><br>
-      <el-input id="pwd" v-model="pwd" placeholder="请输入密码" type="text" class="input"></el-input><br><br>
+        <el-input v-model="user"  placeholder="请输入用户名" type="text" class="input"></el-input><br><br>
+        <el-form :model="Form2" status-icon :rules="rules2" ref="Form2"  class="input">
+        <el-form-item prop="pass">
+          <el-input type="password" v-model="Form2.pass" autocomplete="off" placeholder="请输入密码"></el-input>
+        </el-form-item>
+        <el-form-item prop="checkPass">
+          <el-input type="password" v-model="Form2.checkPass" autocomplete="off" placeholder="请确认密码"></el-input>
+        </el-form-item>
+      </el-form><br><br><br><br>
       <el-input id="email" v-model="email" placeholder="请输入邮箱" type="email" class="input"></el-input><br><br>
-      <el-button type="primary" style="width:250px" @click="getdata" class="input">注 册</el-button><br><br>
+      <el-input placeholder="请输入手机号" type="text" class="input" v-model="num" id="num"></el-input><br><br>
+      <el-button type="primary" style="width:250px" @click="getdata" class="input">注 册</el-button>
       <router-link role="presentation" to="/login"><el-button type="info" style="width:250px" id="btn">已经有账号</el-button></router-link>
-    </div>
+      </div>
   </div>
 </template>
 
 <script>
-  // import $ from 'jquery'
   export default {
     name: "registe",
     data() {
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.Form2.checkPass !== '') {
+            this.$refs.Form2.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.Form2.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
       return {
-        user:'',
-        pwd:'',
+          user:'',
           email:'',
+          num:'',
+        Form2: {
+          pass: '',
+          checkPass: '',
+        },
+        rules2: {
+          pass: [
+            {validator: validatePass, trigger: 'blur'}
+          ],
+          checkPass: [
+            {validator: validatePass2, trigger: 'blur'}
+          ]
+        },
       };
     },
-    methods:{
-     getdata(){
-       let _this=this;
-       $.post('http://localhost:3000/users/addUsers',
-         {
-           userName:_this.user,
-           userPwd:_this.pwd,
-           userEmail:_this.email
-         },function(res){
-           alert('开始注册ok！')
-         })
-     }
-    }
+    methods: {
+      getdata(){
+        if (this.user==''||this.Form2.pass==''||this.email==''||this.num==''){
+          alert('请输入完整信息！！！')
+        }else{
+          let _this = this;
+          $.post('http://localhost:3000/users/addUsers',
+            {
+              userName: _this.user,
+              userPwd: _this.Form2.pass,
+              userEmail: _this.email,
+              userPhoneNum: _this.num
+            }, function (res) {
+              alert('注册完成！！！')
+              _this.$router.push({path:'/'})
+            })
+        }
+
+      },
+    },
   }
+
 </script>
 
 <style scoped>
   .container{
     width:100%;
     height:700px;
-    background-image:url('../../assets/registe.jpg');
+    background:url('../../assets/registe.jpg') no-repeat;
     background-position:-300px;
   }
   .main{
     width:550px;
-    height:600px;
-    margin:70px auto;
+    height:90px;
+    margin:10px auto;
   }
   .login{
     text-align:center;
@@ -58,6 +104,7 @@
     text-align:center;
     margin-top:30px;
   }
+
   .input{
     width:250px;
     height:40px;
--- src/components/user--zhai/user.vue
+++ src/components/user--zhai/user.vue
@@ -2,13 +2,40 @@
   <div class="container">
 
 <span class="title">个人中心</span>
+    <div class="menu">
+      <el-col :span="24">
+        <el-menu
+          default-active="2"
+          @open="handleOpen"
+          @close="handleClose">
+          <el-menu-item index="1">
+            <span slot="title" to="/user">我的资料</span>
+          </el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">
+              <span>我的订单</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">已付款</el-menu-item>
+              <el-menu-item index="1-2">待付款</el-menu-item>
+              <el-menu-item index="1-3">已发货</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-menu-item index="3">
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+    </div>
     <div class="body">
       <div class="pic"></div>
       <router-link role="presentation" to="/changeinfo"><el-button class="btn" >修改资料</el-button></router-link>
       <div class="main">
         <span>用户名：数码达人</span><br><br>
         <span>性别：男</span><br><br>
-        <span>出生年月：1997年1月1日</span><br><br>
         <span>职业：摄影</span><br><br>
         <span>手机号码：18888888888</span><br><br>
         <span>邮箱：mike@163.com</span><br><br>
@@ -20,10 +47,19 @@
 <script>
     export default {
       name: "user",
+      methods:{
+        handleOpen(key, keyPath) {
+          console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+          console.log(key, keyPath);
+        }
+      }
     }
 </script>
 
 <style scoped>
+  *{padding:0;margin:0}
   .container{
     width:100%;
     height:700px;
@@ -36,6 +72,14 @@
     position:absolute;
     margin-left:50px;
     margin-top:80px;
+  }
+  .menu{
+    width:250px;
+    height:500px;
+    position:absolute;
+    float:left;
+    margin-left:15px;
+    margin-top:150px;
   }
   .body{
     width:800px;
